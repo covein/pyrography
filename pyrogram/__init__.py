3c390f85d273789cdcf483323b8f2e534ee0c742
Lelzin λ <d3cryptofc@gmail.com>
2023-06-25 20:32:31 -0300

    Updating copyright notices

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -1,24 +1,35 @@
-#  Pyrogram - Telegram MTProto API Client Library for Python
-#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
-#
-#  This file is part of Pyrogram.
-#
-#  Pyrogram is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Lesser General Public License as published
-#  by the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  Pyrogram is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Pyrography - A wonderful Pyrogram fork inspired by Pyromod & AmanoTeam/Pyrogram.
+Copyright (C) 2023-present Lelzin λ <https://github.com/d3cryptofc>
+
+Forked from Pyrogram <https://github.com/pyrogram/pyrogram>,
+originally copyright (C) 2017-present Dan <https://github.com/delivrance>
+
+This file is part of Pyrography.
+
+Pyrography is is free software: you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+Pyrography is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with Pyrography. If not, see <http://www.gnu.org/licenses/>.
+"""
 
 __version__ = "2.0.106"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
-__copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
+__copyright__ = (
+    'Pyrography - A wonderful Pyrogram fork inspired by Pyromod & AmanoTeam/Pyrogram.\n'
+    'Copyright (C) 2023-present Lelzin λ <https://github.com/d3cryptofc>\n\n'
+
+    'Forked from Pyrogram <https://github.com/pyrogram/pyrogram>,\n'
+    'originally copyright (C) 2017-present Dan <https://github.com/delivrance>'
+)
 
 from concurrent.futures.thread import ThreadPoolExecutor
 
